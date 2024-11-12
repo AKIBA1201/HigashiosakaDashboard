d3788ce54250cefb2a6e37fc31040ee24645a02d
AKIBA1201 <abctake1201@gmail.com>
2024-11-12 12:03:57 +0900

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # app.py
 
 from dash import Dash
-import webbrowser
-from threading import Timer
 
 # アプリケーションの初期化
 app = Dash(__name__)
@@ -13,13 +11,10 @@ from callbacks import register_callbacks
 
 # アプリのレイアウトを設定
 app.layout = layout
-print(app.layout)
 
 # コールバック関数の登録
 register_callbacks(app)
 
 # サーバー起動設定
 if __name__ == '__main__':
-    port = 8051
-    Timer(1, lambda: webbrowser.open(f'http://127.0.0.1:{port}')).start()
-    app.run_server(debug=True, port=port, use_reloader=False)
+    app.run_server(host='0.0.0.0', port=8000, debug=True)
